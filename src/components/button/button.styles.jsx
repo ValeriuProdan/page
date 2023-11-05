9eb187bca9897e12e26ea0eafa78bd0f9ee7c452
Valeriu Prodan <prodan.valeriu96@gmail.com>
2023-11-05 18:48:54 +0100

    Fixed Sign-in and Pictures tabs for mobile

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
 export const BaseButton = styled.button`
-  min-width: 165px;
   width: auto;
-  height: 50px;
+  min-height: 50px;
   letter-spacing: 0.5px;
   line-height: 50px;
   padding: 0 35px 0 35px;
