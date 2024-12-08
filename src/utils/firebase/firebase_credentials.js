7782249f098c76c6d76f7d88a349c3800d91951d
Valeriu Prodan <prodan.valeriu96@gmail.com>
2024-12-08 19:36:24 +0100

    .

---
+++ src/utils/firebase/firebase_credentials.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDO9oJVtGG_nPcKmic91teP3Dc73EHu0cw",
+  authDomain: "webpage-5a874.firebaseapp.com",
+  projectId: "webpage-5a874",
+  storageBucket: "webpage-5a874.appspot.com",
+  messagingSenderId: "993447980166",
+  appId: "1:993447980166:web:b4b3e6e2ca17461fea7507",
+};
